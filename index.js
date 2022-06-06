f8c72bc2428a868a5d9a4dc66cc065b63663da3e
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-06-06 15:33:46 +0300

    worker

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+function Worker(name, tasks = []) {
+    this.name = name;
+
+    const _tasks = tasks; // private
+
+    this.task = function (task) {
+        _tasks.push(task);
+        return this;
+    };
+
+    this.run = function (context, callback) {
+        const tasks = _tasks[Symbol.iterator]();
+        const iterators = [];
+
+        function next() {
+            const job = tasks.next();
+            if (job.done) {
+                return prev();
+            }
+            const iterator = job.value.call(context, next);
+            iterators.unshift(iterator);
+            const task = iterator.next();
+            return task.done ? prev() : task.value(step.bind(null, iterator));
+        }
+
+        function prev() {
+            const iterator = iterators.shift();
+            if (!iterator) {
+                return callback(null, context);
+            }
+            const task = iterator.next();
+            return task.done ? prev() : task.value(step.bind(null, iterator));
+        }
+
+        function step(iterator, err, value) {
+            if (err) {
+                return callback(err);
+            }
+            let task = iterator.next(value);
+            return task.done ? prev() : task.value.call(context, step.bind(null, iterator));
+        }
+
+        next();
+    }
+}
+
+Worker.constructor.name = 'Worker';
+
+module.exports = Worker;
