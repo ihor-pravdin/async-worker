b6c2202dd8026927280a78b6040e4ee7bbaebb74
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-06-27 20:18:25 +0300

    build readme.md

--- build/readme.js
+++ build/readme.js
@@ -4,18 +4,21 @@ const path = require('node:path');
 const readmemd = fs.readFileSync(path.dirname(__dirname) + '/README.md', 'utf8');
 
 // console.log('readmemd:', readmemd);
-console.log('test', "-foo-".match(/-(\w*?)-/));
 console.log('test', ("```foo```" + readmemd).match(/```(\w*?)```/));
 
-const paths = Array.from(readmemd.matchAll(/```(\w*?):(.*?)\s```/gs));
-console.log('match', readmemd.match(/```(\w*?):(.*?)\r\s```/));
-console.log('paths', paths);
+console.log('match', readmemd.match(/```(\w*?):(.*?)\r+\s+```/));
+// const paths = Array.from(readmemd.matchAll(/```(\w*?):(.*?)\r+\s+```/gs));
+// console.log('paths', paths);
 
-const embedded = readmemd.replaceAll(/```(\w*?):(.*?)\r\s```/gs, (match, lang, scriptPath) => {
-    const script = fs.readFileSync(scriptPath, 'utf8');
-    return '```' + lang + ':' + scriptPath + '\r\n' + script + '```';
+const embedded = readmemd.replaceAll(/```(\w*?):(.*?)\r+\s+```/gs, (match, lang, scriptPath) => {
+    const script = path.resolve(path.dirname(__dirname), scriptPath);
+    if (fs.existsSync(script)) {
+        const code = fs.readFileSync(script, 'utf8');
+        return '```' + lang + ':' + scriptPath + '\r\n' + code + '```';
+    }
+    return  match;
 });
 
-console.log('embedded:', embedded);
+// console.log('embedded:', embedded);
 
-//fs.writeFileSync(path.dirname(__dirname) + '/README.md', embedded);
+fs.writeFileSync(path.dirname(__dirname) + '/README.md', embedded);
