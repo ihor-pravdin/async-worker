97b14c33d4b60a61463444f804dcc2656b20ffb0
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-06-27 20:27:25 +0300

    build readme.md

--- build/readme.js
+++ build/readme.js
@@ -4,13 +4,25 @@ const path = require('node:path');
 const readmemd = fs.readFileSync(path.dirname(__dirname) + '/README.md', 'utf8');
 
 // console.log('readmemd:', readmemd);
-console.log('test', ("```foo```" + readmemd).match(/```(\w*?)```/));
+// console.log('test', ("```foo```" + readmemd).match(/```(\w*?)```/));
 
-console.log('match', readmemd.match(/```(\w*?):(.*?)\r+\s+```/));
+// let foo = '```foo```# async-worker\n' +
+//     'Async worker for running iterator on the current event loop with "*down/up execution stream*". \n' +
+//     'Package has zero dependencies and works in browser as well.\n' +
+//     '\n' +
+//     '## Usage\n' +
+//     '\n' +
+//     'Functions that called with `yield` keyword inside generators should take only one argument **callback**. \n' +
+//     'Use `curry` and `callbackify` helpers to make suitable functions with single **callback** arg.  \n' +
+//     '\n' +
+//     '```js:./test/example.js\n' +
+//     '```'
+//
+// console.log('match', foo.match(/`{3}(\w*?):(.*?)\s*`{3}/));
 // const paths = Array.from(readmemd.matchAll(/```(\w*?):(.*?)\r+\s+```/gs));
 // console.log('paths', paths);
 
-const embedded = readmemd.replaceAll(/```(\w*?):(.*?)\r+\s+```/gs, (match, lang, scriptPath) => {
+const embedded = readmemd.replaceAll(/`{3}(\w*?):(.*?)\s*`{3}/gs, (match, lang, scriptPath) => {
     const script = path.resolve(path.dirname(__dirname), scriptPath);
     if (fs.existsSync(script)) {
         const code = fs.readFileSync(script, 'utf8');
